887fe8b516156b24db315be17c1760d477c93247
Stev4iKFeeD <stev4ik.feed@gmail.com>
2022-02-14 23:22:07 +0200

    [Lab03] Add book creation

--- src/main/java/com/example/lab03/controller/BookController.java
+++ src/main/java/com/example/lab03/controller/BookController.java
@@ -1,7 +1,10 @@
 package com.example.lab03.controller;
 
+import com.example.lab03.dto.BookDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -9,6 +12,12 @@ public class BookController {
     @GetMapping("/create-book")
     public String createBookForm() {
         return "create-book";
+    }
+
+    @PostMapping("/create-book")
+    public String createBookConfirm(BookDto bookDto, Model model) {
+        model.addAttribute("book", bookDto);
+        return "books";
     }
 
 }
---
+++ src/main/java/com/example/lab03/dto/BookDto.java
@@ -0,0 +1,5 @@
+package com.example.lab03.dto;
+
+public record BookDto(String title, String isbn, String author) {
+
+}
